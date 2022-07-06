f775a5294c5c0250544b0725f87dc2ba9f83aea0
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-07-06 11:47:50 -0400

    fixup

--- src/CarbonAware.Aggregators/src/CarbonAware/CarbonAwareAggregator.cs
+++ src/CarbonAware.Aggregators/src/CarbonAware/CarbonAwareAggregator.cs
@@ -73,55 +73,54 @@ public class CarbonAwareAggregator : ICarbonAwareAggregator
         }
     }
 
-
-private EmissionsData? GetOptimalEmissions(IEnumerable<EmissionsData> emissionsData)
-{
-    if (!emissionsData.Any())
+    private EmissionsData? GetOptimalEmissions(IEnumerable<EmissionsData> emissionsData)
     {
-        return null;
+        if (!emissionsData.Any())
+        {
+            return null;
+        }
+        return emissionsData.Aggregate((minData, nextData) => minData.Rating < nextData.Rating ? minData : nextData);
     }
-    return emissionsData.Aggregate((minData, nextData) => minData.Rating < nextData.Rating ? minData : nextData);
-}
 
-/// <summary>
-/// Extracts the given offset prop and converts to DateTimeOffset. If prop is not defined, defaults to provided default
-/// </summary>
-/// <param name="props"></param>
-/// <returns>DateTimeOffset representing end period of carbon aware data search. </returns>
-/// <exception cref="ArgumentException">Throws exception if prop isn't a valid DateTimeOffset. </exception>
-private DateTimeOffset GetOffsetOrDefault(IDictionary props, string field, DateTimeOffset defaultValue)
-{
-    // Default if null
-    var dateTimeOffset = props[field] ?? defaultValue;
-
-    // If fail to parse property, throw error
-    if (!DateTimeOffset.TryParse(dateTimeOffset.ToString(), null, DateTimeStyles.AssumeUniversal, out defaultValue))
+    /// <summary>
+    /// Extracts the given offset prop and converts to DateTimeOffset. If prop is not defined, defaults to provided default
+    /// </summary>
+    /// <param name="props"></param>
+    /// <returns>DateTimeOffset representing end period of carbon aware data search. </returns>
+    /// <exception cref="ArgumentException">Throws exception if prop isn't a valid DateTimeOffset. </exception>
+    private DateTimeOffset GetOffsetOrDefault(IDictionary props, string field, DateTimeOffset defaultValue)
     {
-        Exception ex = new ArgumentException("Failed to parse" + field + "field. Must be a valid DateTimeOffset");
+        // Default if null
+        var dateTimeOffset = props[field] ?? defaultValue;
+
+        // If fail to parse property, throw error
+        if (!DateTimeOffset.TryParse(dateTimeOffset.ToString(), null, DateTimeStyles.AssumeUniversal, out defaultValue))
+        {
+            Exception ex = new ArgumentException("Failed to parse" + field + "field. Must be a valid DateTimeOffset");
+            _logger.LogError("argument exception", ex);
+            throw ex;
+        }
+
+        return defaultValue;
+    }
+
+    private IEnumerable<Location> GetLocationOrThrow(IDictionary props)
+    {
+        if (props[CarbonAwareConstants.Locations] is IEnumerable<Location> locations)
+        {
+            return locations;
+        }
+        Exception ex = new ArgumentException("locations parameter must be provided and be non empty");
         _logger.LogError("argument exception", ex);
         throw ex;
     }
 
-    return defaultValue;
-}
-
-private IEnumerable<Location> GetLocationOrThrow(IDictionary props)
-{
-    if (props[CarbonAwareConstants.Locations] is IEnumerable<Location> locations)
+    private TimeSpan GetDurationOrDefault(IDictionary props, TimeSpan defaultValue = default)
     {
-        return locations;
+        if (props[CarbonAwareConstants.Duration] is int duration)
+        {
+            return TimeSpan.FromMinutes(duration);
+        }
+        return defaultValue;
     }
-    Exception ex = new ArgumentException("locations parameter must be provided and be non empty");
-    _logger.LogError("argument exception", ex);
-    throw ex;
-}
-
-private TimeSpan GetDurationOrDefault(IDictionary props, TimeSpan defaultValue = default)
-{
-    if (props[CarbonAwareConstants.Duration] is int duration)
-    {
-        return TimeSpan.FromMinutes(duration);
-    }
-    return defaultValue;
-}
 }
